c133b3484e383f557fb2dda34fe5c3675bc08866
Berthe Szinte <berth.szinte@gmail.com>
2023-08-18 09:03:09 +0200

    change texte items section

--- pages/blog/items.js
+++ pages/blog/items.js
@@ -4,12 +4,18 @@ import Head from "next/head";
 const Items = () => {
   return (
     <>
-    <Head>
-      <title>Items</title>
-    </Head>
-    <Layout>
-      <h1>Items</h1>
-    </Layout>
+      <Head>
+        <title>Items</title>
+      </Head>
+      <Layout>
+        <h1>Items</h1>
+        <ul>
+          <h1>Liste des categories</h1>
+          <li>Culture</li>
+          <li>Social</li>
+          <li>Economique</li>
+        </ul>
+      </Layout>
     </>
   );
 };
